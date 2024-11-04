cad40a71813763d078ee0131ca004107a9d096f0
mmrxsky <mmrxsky@gmail.com>
2024-11-04 18:20:10 +0300

    add исправление ошибок

--- materials/serializer.py
+++ materials/serializer.py
@@ -3,8 +3,8 @@ from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
 
-
 NULLABLE = {"null": True, "blank": True}
+
 
 class LessonSerializer(ModelSerializer):
     class Meta:
@@ -14,7 +14,8 @@ class LessonSerializer(ModelSerializer):
 
 class CourseSerializer(ModelSerializer):
     lessons_count = SerializerMethodField()
-    #lessons = LessonSerializer(source="lesson_set", **NULLABLE)
+
+    # lessons = LessonSerializer(source="lesson_set", **NULLABLE)
     class Meta:
         model = Course
         fields = "__all__"
--- materials/views.py
+++ materials/views.py
@@ -3,7 +3,6 @@ from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      UpdateAPIView)
 from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
 from materials.models import Course, Lesson
 from materials.serializer import CourseSerializer, LessonSerializer
 
@@ -12,25 +11,30 @@ class CourseViewSet(ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
-    class LessonCreateAPIView(CreateAPIView):
-        queryset = Lesson.objects.all()
-        serializer_class = LessonSerializer
 
-    class LessonListAPIView(ListAPIView):
-        queryset = Lesson.objects.all()
-        serializer_class = LessonSerializer
+class LessonCreateAPIView(CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
 
-    class LessonRetrieveAPIView(RetrieveAPIView):
-        queryset = Lesson.objects.all()
-        serializer_class = LessonSerializer
 
-    class LessonUpdateAPIView(UpdateAPIView):
-        queryset = Lesson.objects.all()
-        serializer_class = LessonSerializer
+class LessonListAPIView(ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
 
-    class LessonDestroyAPIView(DestroyAPIView):
-        queryset = Lesson.objects.all()
-        serializer_class = LessonSerializer
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
 
 
 # Create your views here.
