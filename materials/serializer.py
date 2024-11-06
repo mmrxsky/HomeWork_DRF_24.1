35fcd13b64b1f6a45d30772788052e5e2ed01821
mmrxsky <mmrxsky@gmail.com>
2024-11-06 23:18:19 +0300

    add добавил сериализатор для подписки

--- materials/serializer.py
+++ materials/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 from materials.validators import UrlValidator
 
 NULLABLE = {"null": True, "blank": True}
@@ -25,3 +25,8 @@ class CourseSerializer(ModelSerializer):
 
         def get_lessons_count(self, instance):
             return instance.lesson_set.count()
+
+class SubscriptionSerializer(ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
