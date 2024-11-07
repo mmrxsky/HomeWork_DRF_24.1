73faa32970c9748c5f977ae887adab7402e06bde
mmrxsky <mmrxsky@gmail.com>
2024-11-07 16:11:39 +0300

    add исправления

--- materials/urls.py
+++ materials/urls.py
@@ -4,8 +4,7 @@ from rest_framework.routers import SimpleRouter
 from materials.apps import MaterialsConfig
 from materials.views import (CourseViewSet, LessonCreateAPIView,
                              LessonDestroyAPIView, LessonListAPIView,
-                             LessonRetrieveAPIView, LessonUpdateAPIView,
-                             SubscriptionViewSet)
+                             LessonRetrieveAPIView, LessonUpdateAPIView, SubscriptionAPIView)
 
 app_name = MaterialsConfig.name
 
@@ -22,7 +21,7 @@ urlpatterns = [
     path(
         "lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="delete-lesson"
     ),
-    path("subscription/", SubscriptionViewSet.as_view(), name="subscription"),
+    path('subscription/', SubscriptionAPIView.as_view(), name='subscription'),
 ]
 
 urlpatterns += router.urls
